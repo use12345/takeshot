913584495470d4eec84c5274008ee0fd904fd6f7
use12345 <74824012+use12345@users.noreply.github.com>
2021-07-03 12:58:39 +0530

    Add files via upload

---
+++ upload.py
@@ -0,0 +1,46 @@
+import cv2
+import dropbox
+import time
+import random
+
+start_time= time.time()
+
+def take_snapshot():
+    number= random.randint(0,100)
+
+    videoCaptureObject=cv2.VideoCapture(0)
+    result=True
+    while(result):
+        #read the frames while the camera is on
+        ret,frame= videoCaptureObject.read()
+        #cv2.imwrite() method is used to save an image to any storage device
+        img_name= "img" + str(number) + ".png"
+        cv2.imwrite(img_name, frame)
+        start_time=time.time
+        result=False
+    return img_name
+    print("snapshot taken")
+        #release the camera
+    videoCaptureObject.release()
+    #closes all the window that might be opened while this process
+    cv2.destroyAllWindows()
+
+
+def upload_file(img_name):
+    access_token = "J96bvy1hqoUAAAAAAAAAASHYuCcmPOEYRMS_DnPf-qUsZ0Acfkf9J4exPK1F97ER"
+    file =img_name
+    file_from = file
+    file_to="/python/"+(img_name)
+    dbx = dropbox.Dropbox(access_token)
+
+    with open(file_from, 'rb') as f:
+        dbx.files_upload(f.read(), file_to,mode=dropbox.files.WriteMode.overwrite)
+        print("file uploaded")
+        
+def main():
+    while(True):
+        if ((time.time() - start_time) >= 5):
+            name = take_snapshot()
+            upload_file(name)
+
+main()
